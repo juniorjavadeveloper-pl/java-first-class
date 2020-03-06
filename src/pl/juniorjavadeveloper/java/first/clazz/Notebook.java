135a50bfea6f0992292757568be950f29e90dee6
Java Korepetycje <java.korepetycje@gmail.com>
2020-03-06 07:28:13 +0100

    Add first class Notebook with NotebookStore which contains the main() method

---
+++ src/pl/juniorjavadeveloper/java/first/clazz/Notebook.java
@@ -0,0 +1,11 @@
+package pl.juniorjavadeveloper.java.first.clazz;
+
+public class Notebook {
+    int pages;
+    int pagesWritten;
+    int currentPage;
+
+    void goToPage(int page) {
+        currentPage = page;
+    }
+}
---
+++ src/pl/juniorjavadeveloper/java/first/clazz/NotebookStore.java
@@ -0,0 +1,8 @@
+package pl.juniorjavadeveloper.java.first.clazz;
+
+public class NotebookStore {
+    public static void main(String[] args) {
+        Notebook blankNotebook = new Notebook();
+        Notebook personalNotebook = new Notebook();
+    }
+}
